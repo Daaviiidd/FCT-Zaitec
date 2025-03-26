c7b5559605e4c0629554f065f3090578e0416fc1
cac0019 <cac0019@alu.medac.es>
2025-03-26 11:07:06 +0100

    Actualización de gradle

--- fct_inventario/android/app/build.gradle.kts
+++ fct_inventario/android/app/build.gradle.kts
@@ -1,47 +1,47 @@
 plugins {
     id("com.android.application")
-    id("com.google.gms.google-services") // Plugin de Firebase
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
-    id("dev.flutter.flutter-gradle-plugin") // Plugin de Flutter
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.example.fct_inventario"
-    compileSdk = 34  // Asegúrate de usar una versión compatible
-    ndkVersion = "25.2.9519653" // Usa la versión de tu NDK si es necesaria
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.fct_inventario"
-        minSdk = 21  // Asegúrate de que tu versión mínima es compatible con Firebase
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
 
 flutter {
     source = "../.."
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.firebase:firebase-auth-ktx:22.1.1")  // Firebase Auth
-    implementation("com.google.firebase:firebase-firestore-ktx:24.9.0")  // Firestore
 }
